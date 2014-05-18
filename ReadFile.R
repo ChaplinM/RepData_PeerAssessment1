e9c7f83a63be3d92f73cea479c762a01546d0e03
chaplinm <chaplinmarc@gmail.com>
2014-05-19 00:28:34 +0200

    First full version

--- ReadFile.R
+++
@@ -1,8 +0,0 @@
-library(data.table)
-
-#activity = read.csv("activity/activity.csv", header=T)
-activity = read.table("activity/activity.csv", sep=",", header=T)
-str(activity) # 17568 obs. of 3 variables etc.
-summary(activity$steps) # NA's: 2304
-
-class(activity) # "data.frame"
